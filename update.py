25df04942dfbabc76ee6e8b0a13b78870d1b33fa
Leonid <leonbest2006@gmail.com>
2025-05-27 00:33:42 +0300

    Update update.py

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ from bd import AsyncSessionLocal, select, Habit, Habit_progress, User
 from send_reminder import send_reminder,check_strick
 
 
-# Функция для создания записи о прогрессе привычек
+
 async def create_new_habit_progress():
     today = datetime.date.today()
     try:
@@ -28,11 +28,9 @@ async def create_new_habit_progress():
         print(f"[{today}] ❌ Error while creating progress records:", str(e))
 
 
-# Функция для запуска планировщика
 def start_scheduler():
     scheduler = AsyncIOScheduler()
 
-    # Запуск каждый день в 00:00
     scheduler.add_job(create_new_habit_progress, CronTrigger(hour=0, minute=0))
 
     scheduler.add_job(send_reminder, CronTrigger(hour=22, minute=0))

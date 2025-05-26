a3da0703b25c50ff6cd1a634d62b24dc115a66c4
Leonid <leonbest2006@gmail.com>
2025-05-27 00:33:12 +0300

    Update send_reminder.py

--- send_reminder.py
+++ send_reminder.py
@@ -16,22 +16,18 @@ async def send_reminder():
 
 async def check_strick():
     async with AsyncSessionLocal() as session:
-        # Извлекаем все прогрессы по привычкам, сортируем по дате
         result = await session.execute(select(Habit_progress).order_by(Habit_progress.date.desc()))
         habits_progress = result.scalars().all()
         
-        # Извлекаем всех пользователей
         users = await session.execute(select(User))
         users = users.scalars().all()
 
-        streaks = {}  # Словарь для хранения стриков для каждой привычки
+        streaks = {} 
 
-        # Проходим по всем записям прогресса
         for habit_progress in habits_progress:
             habit_id = habit_progress.habit_id
-            user_id = habit_progress.habit.user_id  # Получаем пользователя для данной привычки
+            user_id = habit_progress.habit.user_id  
 
-            # Начинаем или обновляем стрик для привычки
             if habit_progress.is_done:
                 if habit_id not in streaks:
                     streaks[habit_id] = 1
@@ -39,11 +35,8 @@ async def check_strick():
                     streaks[habit_id] += 1
             else:
                 streaks[habit_id] = 0
-
-            # Если стрик больше или равен 7 дням, отправляем сообщение
             if streaks[habit_id] >= 7:
-                # Отправляем сообщение пользователю
-                user = next((u for u in users if u.id == user_id), None)  # Находим пользователя по id
+                user = next((u for u in users if u.id == user_id), None)
                 if user:
                     await bot.send_message(chat_id=user.tg_id, text="You have reached a 1-week streak!!!")
 
